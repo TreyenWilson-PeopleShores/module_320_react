7090b967e17007bbde10e433d7cb40e144bc216b
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-18 16:31:55 -0500

    Added proper information

--- lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/App.jsx
+++ lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/App.jsx
@@ -5,7 +5,8 @@ const employee = {
   firstName: "Julie",
   lastName: "Taylor",
   title: "VP of Marketing",
-  phone: "781-000-0002",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
   email: "jtaylor@fakeemail.com",
   picture: "https://randomuser.me/api/portraits/women/2.jpg",
 };
--- lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/components/EmployeePage.jsx
+++ lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/components/EmployeePage.jsx
@@ -1,40 +1,42 @@
 import "./EmployeePage.css";
 function EmployeeCard({ employee }) {
   return (
-    <div className="card">
-      {/* Profile section */}
-      <div className="card__profile">
-          <img
-            className="card__photo"
-            src={employee.picture}
-            alt={employee.firstName + " " + employee.lastName}
-          />
-        <div className="personalInfo"> 
-          <h2 className="card__name">
-            {employee.firstName} {employee.lastName}
-          </h2>
-          <p className="card__title">{employee.title}</p>
+    <div className = "div-container">
+      <div className="card">
+        {/* Profile section */}
+        <div className="card__profile">
+            <img
+              className="card__photo"
+              src={employee.picture}
+              alt={employee.firstName + " " + employee.lastName}
+            />
+          <div className="personalInfo"> 
+            <h2 className="card__name">
+              {employee.firstName} {employee.lastName}
+            </h2>
+            <p className="card__title">{employee.title}</p>
+          </div>
         </div>
+        {/* Contact details */}
+        <p className="card__details">
+          <ol className="card__row">
+            <span className="card__label">Call Office</span>
+            <span className="card__value">{employee.officePhone}</span>
+          </ol>
+          <ol className="card__row">
+            <span className="card__label">Call Mobile</span>
+            <span className="card__value">{employee.phone}</span>
+          </ol>
+          <ol className="card__row">
+            <span className="card__label">SMS</span>
+            <span className="card__value">{employee.phone}</span>
+          </ol>
+          <ol className="card__row">
+            <span className="card__label">Email</span>
+            <span className="card__value">{employee.email}</span>
+          </ol>
+        </p>
       </div>
-      {/* Contact details */}
-      <p className="card__details">
-        <ol className="card__row">
-          <span className="card__label">Call Office</span>
-          <span className="card__value">{employee.phone}</span>
-        </ol>
-        <ol className="card__row">
-          <span className="card__label">Call Mobile</span>
-          <span className="card__value">{employee.phone}</span>
-        </ol>
-        <ol className="card__row">
-          <span className="card__label">SMS</span>
-          <span className="card__value">{employee.email}</span>
-        </ol>
-        <ol className="card__row">
-          <span className="card__label">Email</span>
-          <span className="card__value">{employee.email}</span>
-        </ol>
-      </p>
     </div>
   );
 }
