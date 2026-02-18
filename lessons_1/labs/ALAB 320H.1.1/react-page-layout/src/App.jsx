a97593619ff7a59bfb811c2d75aa2592cc9903e5
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-18 17:00:00 -0500

    Finished EmployeePage

--- lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/App.jsx
+++ lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/App.jsx
@@ -1,15 +1,8 @@
 import EmployeeCard from "./components/EmployeePage";
 import "./App.css";
+import employee from "./information/EmployeeInformation"
 // Employee data — just a plain JavaScript object
-const employee = {
-  firstName: "Julie",
-  lastName: "Taylor",
-  title: "VP of Marketing",
-  officePhone: "781-000-0002",
-  phone: "617-000-0002",
-  email: "jtaylor@fakeemail.com",
-  picture: "https://randomuser.me/api/portraits/women/2.jpg",
-};
+
 function App() {
   return (
     <div className="app">
---
+++ lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/information/EmployeeInformation.jsx
@@ -0,0 +1,14 @@
+const employee = {
+  firstName: "Julie",
+  lastName: "Taylor",
+  title: "VP of Marketing",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
+  email: "jtaylor@fakeemail.com",
+  picture: "https://randomuser.me/api/portraits/women/2.jpg",
+};
+
+
+
+
+export default employee
