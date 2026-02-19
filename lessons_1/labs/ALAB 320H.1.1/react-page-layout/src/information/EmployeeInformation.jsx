357ea94c527a229db30d12210ba76d2852d828ef
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-19 12:03:48 -0500

    Need to updated EmployeeInformationList

--- lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/information/EmployeeInformation.jsx
+++ lessons_1/labs/ALAB 320H.1.1/react-page-layout/src/information/EmployeeInformation.jsx
@@ -6,6 +6,46 @@ const employee = [{
   phone: "617-000-0002",
   email: "jtaylor@fakeemail.com",
   picture: "https://randomuser.me/api/portraits/women/2.jpg",
+},{
+  firstName: "Julie",
+  lastName: "Taylor",
+  title: "VP of Marketing",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
+  email: "jtaylor@fakeemail.com",
+  picture: "https://randomuser.me/api/portraits/women/2.jpg",
+},,{
+  firstName: "Julie",
+  lastName: "Taylor",
+  title: "VP of Marketing",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
+  email: "jtaylor@fakeemail.com",
+  picture: "https://randomuser.me/api/portraits/women/2.jpg",
+},{
+  firstName: "Julie",
+  lastName: "Taylor",
+  title: "VP of Marketing",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
+  email: "jtaylor@fakeemail.com",
+  picture: "https://randomuser.me/api/portraits/women/2.jpg",
+},{
+  firstName: "Julie",
+  lastName: "Taylor",
+  title: "VP of Marketing",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
+  email: "jtaylor@fakeemail.com",
+  picture: "https://randomuser.me/api/portraits/women/2.jpg",
+},{
+  firstName: "Julie",
+  lastName: "Taylor",
+  title: "VP of Marketing",
+  officePhone: "781-000-0002",
+  phone: "617-000-0002",
+  email: "jtaylor@fakeemail.com",
+  picture: "https://randomuser.me/api/portraits/women/2.jpg",
 }];
 
 
